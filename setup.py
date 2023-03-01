3e1c9c65f7085935ad8a1ba560e91d1564da37d7
Faisal ALSHARGI <alshargi@hotmail.de>
2023-03-01 13:36:00 -0500

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+ 
+setuptools.setup(
+    name='AutoLocalizationTask',
+    version='0.0.1',
+    author='Faisal Alshargi',
+    author_email='falsharg@amazon.com',
+    description='Auto Localization Task package',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/alshargi/AutoLocalizationTask',
+    project_urls = {
+        "Bug Tracker": "https://github.com/alshargi/AutoLocalizationTask/issues"
+    },
+    license='MIT',
+    packages=['AutoLocalizationTask'],
+    package_data={'AutoLocalizationTask': ['models/*.txt', 'models/*.sav']}, 
+    install_requires=['requests'],
+)
